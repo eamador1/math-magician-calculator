a0f5e184754498d3fceaa19a375a5f8b12e02397
Erwin Amador <eamador0305@gmail.com>
2023-09-20 16:13:36 -0600

    Create structure for calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import './App.css';
+import Button from './components/Calculator';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <div className="calculator-container">
+        <div className="line">
+          <Button>AC</Button>
+          <Button>+/-</Button>
+          <Button>%</Button>
+          <Button>÷</Button>
+        </div>
+        <div className="line">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>x</Button>
+        </div>
+
+        <div className="line">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6 </Button>
+          <Button>-</Button>
+        </div>
+        <div className="line">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className="line">
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>=</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
