3c628dee12c677e4e17e998a2f3eb366a2622bf7
Erwin Amador <eamador0305@gmail.com>
2023-09-20 20:19:48 -0600

    Create button component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/calculator.css';
 
-function Button({children}) {
+function Button({ children }) {
+  const isOperator = (value) => value === '÷' || value === '-' || value === '+' || value === 'x' || value === '=';
   return (
-    <button>{children}</button>
+    <div
+      className={`button-container ${isOperator(children) ? 'operator' : ''}`.trimEnd()}
+    >
+      {children}
+    </div>
+
   );
 }
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired, // Define the 'id' prop type as a required string
+};
 
 export default Button;
