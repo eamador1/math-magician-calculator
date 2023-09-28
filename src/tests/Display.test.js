4e815ebbee496927590f864fc81c3b57db9d4e19
JOSE RM <mojica.raul@gmail.com>
2023-09-28 09:48:48 -0600

    Create Display.test.js

---
+++ src/tests/Display.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Screen from '../components/Display’;
+
+import { render } from '@testing-library/react';
+
+describe('Screen Component', () => {
+it('renders input', () => {
+    const { getByText } = render(<Screen input='1' />);
+    
+    // Use React Testing Library to check if the component renders the input text correctly
+    const screenElement = getByText('1');
+    
+    // Assert that the screen element is in the document
+
+    expect(screenElement).toBeInTheDocument();
+    expect(screenElement).toMatchSnapshot();
+})
+it('renders input', () => {
+    const { getByText } = render(<Screen input='99.9' />);
+    
+    // Use React Testing Library to check if the component renders the input text correctly
+    const screenElement = getByText('99.9');
+    
+    // Assert that the screen element is in the document
+
+    expect(screenElement).toBeInTheDocument();
+    expect(screenElement).toMatchSnapshot();
+})
+});
